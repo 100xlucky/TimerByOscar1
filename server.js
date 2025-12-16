c6d3a2c7c71f8fd490a97403eec5134c9a155696
beniardiansyah846-max <beniardiansyah846@gmail.com>
2025-12-16 18:14:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,6 @@
-const express = require("express");
-const http = require("http");
-const WebSocket = require("ws");
-
-const app = express();
-app.use(express.static("public"));
-
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", ws => {
-  ws.on("message", msg => {
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(msg);
-      }
-    });
-  });
-});
-
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-  console.log("Server running on", PORT);
+  console.log("Server running on port", PORT);
 });
+
