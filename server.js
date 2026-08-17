62f5afd63b57eca5d3e65e96ac59cfb5730aced2
100xlucky <umutbaba7878e@gmail.com>
2026-08-17 23:28:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express');
-const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
+const { WebcastPushConnection } = require('tiktok-live-connector');
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-// Public klasöründeki dosyaları dışarı sunar
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-// Herhangi bir adrese girildiğinde direkt index.html'i açar
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+io.on('connection', (socket) => {
+    let tiktokConnection;
+
+    socket.on('setUniqueId', (uniqueId) => {
+        if (tiktokConnection) {
+            tiktokConnection.disconnect();
+        }
+
+        tiktokConnection = new WebcastPushConnection(uniqueId);
+
+        tiktokConnection.connect().then(state => {
+            socket.emit('streamData', {
+                viewerCount: state.viewerCount
+            });
+        }).catch(err => {
+            socket.emit('error', 'Yayına bağlanılamadı');
+        });
+
+        tiktokConnection.on('roomUser', msg => {
+            socket.emit('updateViewers', msg.viewerCount);
+        });
+
+        tiktokConnection.on('envelope', data => {
+            socket.emit('chestEvent', data);
+        });
+    });
+
+    socket.on('disconnect', () => {
+        if (tiktokConnection) tiktokConnection.disconnect();
+    });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Sunucu ${PORT} portunda aktif.`));
